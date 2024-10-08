4952b7a96b5468247f06f66e3b635a63a8f5786b
lpm0073 <lpm0073@gmail.com>
2024-10-08 17:17:53 -0600

    chore: rename tutor plugin to indigo-stepwisemath

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     python_requires=">=3.8",
     install_requires=["tutor>=18.0.0,<19.0.0", "tutor-mfe>=18.0.0,<19.0.0"],
     extras_require={"dev": "tutor[dev]>=18.0.0,<19.0.0"},
-    entry_points={"tutor.plugin.v1": ["indigo = tutorindigo.plugin"]},
+    entry_points={"tutor.plugin.v1": ["indigo-stepwisemath = tutorindigo.plugin"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
